f91377129392a92619624456edb7a9a6a23a8e7b
kjone8371 <rlawjddnsj8371@gmail.com>
2024-04-18 09:52:59 +0900

    1주차

---
+++ src/main/java/com/kjone/kjonespringbootjpaproject/KjoneSpringBootJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kjone.kjonespringbootjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjoneSpringBootJpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjoneSpringBootJpaProjectApplication.class, args);
+    }
+
+}
