f630e56c1bbd69ae13d5653bf5aa7d1f48b48dda
kjone8371 <kjone8371@gmail.com>
2024-04-25 10:57:45 +0900

    DB table 제작

--- src/main/java/com/kjone/kjonespringbootjpaproject/domain/role/Role.java
+++ src/main/java/com/kjone/kjonespringbootjpaproject/domain/role/Role.java
@@ -2,7 +2,6 @@ package com.kjone.kjonespringbootjpaproject.domain.role;
 
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @ToString
--- src/main/java/com/kjone/kjonespringbootjpaproject/repository/UserRepository.java
+++ src/main/java/com/kjone/kjonespringbootjpaproject/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.kjone.kjonespringbootjpaproject.repository;
 
+import com.kjone.kjonespringbootjpaproject.domain.user.SignRequest;
 import com.kjone.kjonespringbootjpaproject.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByEmail(String email);
+    Optional<SignRequest> findByRequestedId(String id);
+
 }
